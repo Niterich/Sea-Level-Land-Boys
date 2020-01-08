fc70b79e5be253154989a21ad2d455a3691e3761
Niterich <52295078+Niterich@users.noreply.github.com>
2020-01-07 21:32:26 -0700

    got css and js working together

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,15 @@
-
+console.log("connected");
 let quotePic;
 let quoteCategory;
 let value;
 
 $(".quoteGenButton").on("click", function() {
+    console.log("clicked")
     clearDiv();
     value = $(this).val();
     console.log(value);
     $.ajax({
-        url: "http://quotes.rest/qod.json?category=" + value,
+        url: "https://quotes.rest/qod.json?category=" + value,
         method: "GET"
     }).then(function(res) {
         console.log(res);
@@ -16,13 +17,22 @@ $(".quoteGenButton").on("click", function() {
         $("#quoteBenderDisplayDiv").append(quotePic);
     });
     $.ajax({
-        url: "https://newsapi.org/v2/top-headlines?q="+ value +"&apiKey=83aa0452f72948bf8d4ed15cf295b532",
+        url: "https://newsapi.org/v2/top-headlines?q=" + value + "&apiKey=83aa0452f72948bf8d4ed15cf295b532",
         method: "GET"
     }).then(function(resTwo){
         console.log(resTwo);
-    })
+        for (let i = 0; i < resTwo.articles.length; i++) {
+            const articleCard = $("<div>")
+            const articleTitle = $("<h3>").text(resTwo.articles[i].title).appendTo(articleCard);
+            const articlePic = $("<img>").attr("src", resTwo.articles[i].urlToImage).appendTo(articleCard);
+            const articleDescription = $("<p>").text(resTwo.articles[i].description).appendTo(articleCard);
+            const articleAuthor = $("<p>").text(resTwo.articles[i].author).appendTo(articleCard);
+            const articleUrl = $("<a>").text("Link to article").attr("href", resTwo.articles[i].url).appendTo(articleCard);
+            articleCard.addClass("card col-8");
+            $("#articles").append(articleCard);
+        };
+    });
 });
-
 function clearDiv(){
 	$("#quoteBenderDisplayDiv").empty();
 };
