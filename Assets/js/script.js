573f979b2593f224bb163bba5dc37a1374f66439
Niterich <52295078+Niterich@users.noreply.github.com>
2020-01-07 19:21:44 -0700

    worked on second ajax call

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,70 +1,28 @@
-let generatedQuote;
+
 let quotePic;
-let author;
+let quoteCategory;
+let value;
 
 $(".quoteGenButton").on("click", function() {
-    const value = $(this).val();
+    clearDiv();
+    value = $(this).val();
     console.log(value);
     $.ajax({
         url: "http://quotes.rest/qod.json?category=" + value,
         method: "GET"
     }).then(function(res) {
         console.log(res);
-		// generatedQuote = $("<p>").text(res.contents.quotes[0].quote);
 		quotePic = $("<img>").attr("src", "https://theysaidso.com/quote/image/" + res.contents.quotes[0].id);
-		// author = $("<p>").text("- " + res.contents.quotes[0].author)
-		// $("#quoteBenderDisplayDiv").append(generatedQuote);
-		// $("#quoteBenderDisplayDiv").append(author)
-		$("#quoteBenderDisplayDiv").append(quotePic);
-        // $("#generatorButtonWrapper").addClass("hide");
-        // $("#benderButtonWrapper").removeClass("hide");
+        $("#quoteBenderDisplayDiv").append(quotePic);
     });
-    // $(".quoteBendButton").on("click", function() {
-    //     const value = $(this).val();
-    //     console.log(value);
-    //     $.ajax({
-    //         url: "https://api.funtranslations.com/translate/" + value + ".json?text=" + generatedQuote,
-    //         method: "GET"
-    //     }).then(function(resTwo) {
-    //         console.log(resTwo);
-    //     });
-    // });
+    $.ajax({
+        url: "https://newsapi.org/v2/top-headlines?q="+ value +"&apiKey=83aa0452f72948bf8d4ed15cf295b532",
+        method: "GET"
+    }).then(function(resTwo){
+        console.log(resTwo);
+    })
 });
 
 function clearDiv(){
 	$("#quoteBenderDisplayDiv").empty();
-}
-
-// $(".qod").on("click", function() {
-//     $.ajax({
-//         url: "http://quotes.rest/qod.json",
-//         method: "GET"
-//     }).then(function(res) {
-//         console.log(res);
-// 		generatedQuote = $("<p>").text(res.contents.quotes[0].quote);
-// 		quotePic = $("<img>").attr("src", res.contents.quotes[0].background);
-// 		$("#quoteBenderDisplayDiv").append(quotePic);
-//         $("#quoteBenderDisplayDiv").append(generatedQuote);
-//         $("#generatorButtonWrapper").addClass("hide");
-//         $("#benderButtonWrapper").removeClass("hide");
-//     });
-//     $(".quoteBendButton").on("click", function() {
-//         const value = $(this).val();
-//         console.log(value);
-//         $.ajax({
-//             url: "https://api.funtranslations.com/translate/" + value + ".json?text=" + generatedQuote,
-//             method: "GET"
-//         }).then(function(resTwo) {
-//             console.log(resTwo);
-//         });
-//     });
-// });
-
-// $("#submitCustomQuoteButton").on("click", function(){
-// 	event.preventDefault();
-// 	generatedQuote = $("<p>").text($(this).prev().val());
-//     $("#quoteBenderDisplayDiv").append(generatedQuote);
-//     $("#generatorButtonWrapper").addClass("hide");
-//     $("#benderButtonWrapper").removeClass("hide");
-
-// })
+};
